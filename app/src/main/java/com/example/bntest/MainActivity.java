27883af4bdbd4e092b0613fe5ddf3f1e7e0f0abc
immsee098 <immsee098@gmail.com>
2020-04-01 09:51:16 +0900

    Initial commit

---
+++ app/src/main/java/com/example/bntest/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.bntest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @BindView(R.id.helloText)
+    TextView helloText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this); //class.this
+
+
+    }
+}
